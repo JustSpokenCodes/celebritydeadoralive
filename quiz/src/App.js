df59319ee225b36b07ed03dcd5da39a2729c5b23
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-09-06 12:40:18 -0700

    start over

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Question from './components/Question.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>Celebrity Dead or Alive!?</h2>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
-      <Question content="Is this person Alive or Dead?" />
     </div>
   );
 }
